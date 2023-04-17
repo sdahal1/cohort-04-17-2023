291de24130ccf29a5a15f67666d36124a0cd77ce
sdahal1 <raabdahl@gmail.com>
2023-04-17 11:52:33 -0700

    initial commit

---
+++ 02.1-02.5/javascriptDebugger.js
@@ -0,0 +1,6 @@
+let languages = ["HTML", "CSS", "JavaScript"];
+
+for (let i = 0; i < languages.length; i++) {
+  let language = languages[i];
+  console.log("I am excited to learn " + language);
+}
