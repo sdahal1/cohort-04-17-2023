4f8bc92b3f2dfa3eed45101fb7ea4e744a840750
sdahal1 <raabdahl@gmail.com>
2023-04-26 08:06:11 -0700

    w2d3am

--- 06.1-local-library-study-guide/src/demo.js
+++ 06.1-local-library-study-guide/src/demo.js
@@ -61,11 +61,70 @@ function sortStudentsByFirstName(students) {
 //7. Partition courses by student on pace
 //find the courses that have all students on pace and find courses that have at least one student not on pace. Return back an array containing 2 sub-arrays inside it. First sub-array will have the courses where all students are on pace, and the second sub-array will have the courses where not all students are on pace
 
-function partitionCoursesByStudentProgress(courses) {
-    
+
+
+function partitionCoursesByStudentProgress(courses=[]) {
+    //create array for onPace courses
+    const onPace = [];
+    //create array for notOnPace Courses
+    const notOnPace = [];
+    //loop through the courses array
+    courses.forEach((courseObj)=>{
+        //get access to roster for each course
+        const {roster} = courseObj;
+        //have flag to represent if the courseObj is on pace or not
+        let isOnPace = true;
+        //loop through the roster
+        roster.forEach((rosterObj)=>{
+            //for each element we will look at onPace. if everyone is on pace, we will push this to the "onPaceCourses array" otherwise we will push the current course to notOnPaceCourses array
+            if(rosterObj.onPace === false){
+                //toggle the flag to false
+                isOnPace = false
+                // return;
+            }
+        })
+
+        if(isOnPace === true){
+            onPace.push(courseObj)
+        }else{
+            notOnPace.push(courseObj)
+        }
+    })
+    return [onPace,notOnPace]
 }
 
-// console.log(partitionCoursesByStudentProgress(courses))
+
+function partitionCoursesByStudentProgress2(courses=[]) {
+    //filter the courses on pace
+    const onPaceCourses = courses.filter((courseObj)=>{
+        //get access to roster for each course
+        const {roster} = courseObj;
+
+        //check if every element in roster has onPace === true
+        const isOnPace = roster.every((rosterObj)=>{
+            return rosterObj.onPace === true
+        })
+
+        return isOnPace;
+    })
+    //filter the courses not on pace
+    const notOnPace = courses.filter((courseObj)=>{
+        //get access to roster for each course
+        const {roster} = courseObj;
+
+        //check if every element in roster has onPace === true
+        const isNotOnPace = roster.some((rosterObj)=>{
+            return rosterObj.onPace === false
+        })
+
+        return isNotOnPace;
+    })
+
+    return [onPaceCourses, notOnPace]
+}
+
+
+// console.log(partitionCoursesByStudentProgress2(courses))
 
 /* 
 
@@ -101,8 +160,23 @@ let oneCourse = {
 }
 */
 
-function getStudentsForCourse(course, students) {
-    
+function getStudentsForCourse(course={}, students=[]) {
+    const {roster} = course;
+    const result = [];
+    //loop through the roster of the given course
+    roster.forEach((rosterObj)=>{
+        //look at the id of the rosterObj so we can match it with the id of a student in the students array
+        // rosterObj.studentId
+        let foundStudent = undefined;
+        //loop through the students array
+        students.forEach((studentObj)=>{
+            if(studentObj.id === rosterObj.studentId){
+                foundStudent = studentObj
+            }
+        })
+        result.push(foundStudent)
+    })
+    return result;
 }
 
 let oneCourse = {
@@ -115,26 +189,26 @@ let oneCourse = {
             studentId: 1,
             onPace: true,
         },
-        {
-            studentId: 2,
-            onPace: false,
-        },
-        {
-            studentId: 3,
-            onPace: true,
-        },
-        {
-            studentId: 4,
-            onPace: true,
-        },
-        {
-            studentId: 5,
-            onPace: true,
-        },
+        // {
+        //     studentId: 2,
+        //     onPace: false,
+        // },
+        // {
+        //     studentId: 3,
+        //     onPace: true,
+        // },
+        // {
+        //     studentId: 4,
+        //     onPace: true,
+        // },
+        // {
+        //     studentId: 5,
+        //     onPace: true,
+        // },
     ],
 };
 
-// console.log(getStudentsForCourse(oneCourse, students))
+console.log(getStudentsForCourse(oneCourse, students))
 
 /* 
 9. getTotalNumberOfClassesForStudent- Given a student object and an array of course objects, find the number of times this student object's id appears in the all the courses rosters array
