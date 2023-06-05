12dd2922934d56e20bf65cc3f1bf8377dc718312
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-06-05 08:00:53 -0700

    add abortcontrollers

--- 24-hooks-apis-fetch/tv-showy/src/App.js
+++ 24-hooks-apis-fetch/tv-showy/src/App.js
@@ -14,22 +14,49 @@ function App() {
   // step 1: a state variable to hold the data
   const [shows, setShows] = useState([]);
   // step 2: a useEffect to make the request (don't forget a dependency array!)
-  // useEffect(() => {
-  //   // step 3: make the request & save the data from the response into the state variable
-  //   fetch('https://api.tvmaze.com/shows')
-  //     .then(response => response.json())
-  //     .then(data => setShows(data));
-  // }, [])
+  useEffect(() => {
+    // step 3: make the request & save the data from the response into the state variable
+    const abortController = new AbortController();
+    fetch('https://api.tvmaze.com/shows', { signal: abortController.signal })
+      .then(response => response.json())
+      .then(data => setShows(data))
+      .catch(e => {
+        if (e.name === 'AbortError') {
+          // this was the expected error, log it and carry on
+          console.log('Aborted')
+        } else {
+          throw e;
+        }
+      });
+    return () => {
+      abortController.abort();
+    }
+  }, [])
 
   // or, with async/await
-  useEffect(() => {
-    async function fetchShows() {
-      let response = await fetch('https://api.tvmaze.com/shows');
-      let data = await response.json();
-      setShows(data);
-    }
-    fetchShows();
-  }, [])
+  // useEffect(() => {
+  //   const abortController = new AbortController();
+  //   async function fetchShows() {
+  //     try {
+  //       let response = await fetch('https://api.tvmaze.com/shows', { signal: abortController.signal });
+  //       let data = await response.json();
+  //       setShows(data);
+  //     } catch (e) {
+  //       if (e.name === 'AbortError') {
+  //         // this was the expected error, log it and carry on
+  //         console.log('Aborted')
+  //       } else {
+  //         throw e;
+  //       }
+  //     }
+  //   }
+  //   fetchShows();
+  //   return () => {
+  //     // code inside of this function gets run only if we need to clean up
+  //     // so, if we need to clean up, we should abort the fetch
+  //     abortController.abort();
+  //   }
+  // }, [])
 
   return (
     <div>
