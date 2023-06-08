07b3bed77482feabe5a6b564560d52112c4c3375
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-06-08 08:15:18 -0700

    fix image rendering

--- 25-react-router/tv-showy/src/Show.js
+++ 25-react-router/tv-showy/src/Show.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 function Show({ show }) {
   return <div>
     <Link to={`/tvshows/${show.id}`}><h3>{show.name}</h3></Link>
-    <img src={show.image.medium} />
+    {show.image && <img src={show.image.medium} />}
     <p>Genres: {show.genres.join(', ')}</p>
   </div>
 }
