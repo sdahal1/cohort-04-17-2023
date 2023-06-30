ff7a3fe132e4fdc314811a7a02e9e779221c628b
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-06-30 10:53:01 -0700

    candies can be rendered to the page

--- 35-postgres-node-express/candies-frontend/src/App.js
+++ 35-postgres-node-express/candies-frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Candy from "./Candy";
 
 function App() {
+  const [candies, setCandies] = useState([{
+    id: 1,
+    name: 'mnms',
+    sweetness: 9
+  },
+  {
+    id: 2,
+    name: 'snickers',
+    sweetness: 7
+  }])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Candies!</h1>
+      {candies.map(candy => <Candy candy={candy} key={candy.id} />)}
     </div>
   );
 }
